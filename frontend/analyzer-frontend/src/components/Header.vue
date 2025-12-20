4ce60dfeba11c39384c471d20c83b6a9e7a8f6f1
Max <Maximilianlakke@googlemail.com>
2025-12-20 12:33:23 +0100

    feat: add fire.svg asset and enhance toast notifications with success messages

--- frontend/analyzer-frontend/src/components/Header.vue
+++ frontend/analyzer-frontend/src/components/Header.vue
@@ -5,16 +5,54 @@
                 <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="32" height="32" />
                 <h1 class="title">Threema Chat Analyzer</h1>
             </div>
+            <div class="header-right">
+                <a class="additional-link" @click="handleRemoveData" target="_blank" rel="noopener noreferrer" title="Remove Data">
+                    <img src="../assets/fire.svg" alt="Additional" class="github-icon" />
+                </a>
+                <a class="github-link"  href="https://github.com/maxiking445/threema-chat-analyzer" target="_blank"
+                    rel="noopener noreferrer">
+                    <img src="../assets/github.svg" alt="GitHub" class="github-icon" />
+                </a>
 
-            <a class="github-link" href="https://github.com/maxiking445/threema-chat-analyzer" target="_blank"
-                rel="noopener noreferrer">
-                <img src="../assets/github.svg" alt="GitHub" class="github-icon" />
-            </a>
+            </div>
         </header>
     </div>
 </template>
 
+<script setup>
+import { UploadApi } from '@/generated/api';
+import { pushErrorToast } from '@/service/ToastService'
+import { pushSuccessToast } from '@/service/ToastService'
+
+
+  
+const uploadApi = new UploadApi();
+
+const handleRemoveData = () => {
+    uploadApi.deleteZipDelete().then(() => {
+        pushSuccessToast('Data removed successfully');
+        console.log('Data removed successfully');
+    }).catch((error) => {
+        pushErrorToast(error.response?.data || 'Error removing data');
+        console.error('Error removing data:', error);
+    });
+}
+
+</script>
 <style scoped>
+.header-right {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+}
+
+.additional-link {
+    display: inline-flex;
+    align-items: center;
+    filter: invert(1);
+}
+
+
 .github-link {
     filter: invert(1);
 }
