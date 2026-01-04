8c1272a1a96553ebe9399fd3a4d8123c19e426a5
Max <Maximilianlakke@googlemail.com>
2026-01-04 18:12:23 +0100

    fix: refactor fixes

--- frontend/analyzer-frontend/src/components/GroupSelect.vue
+++ frontend/analyzer-frontend/src/components/GroupSelect.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <ViewPanelTemplate :title="`Group Activity: ${props.selectedGroup.groupName}`" :key="props.selectedGroup?.id">
+    <ViewPanelTemplate
+      :title="props.selectedGroup?.groupName ? `Group Activity: ${props.selectedGroup.groupName}` : 'Group Activity:'"
+      :key="props.selectedGroup?.id">
       <div v-if="selectedGroup.groupMember == undefined">
-       <NoData></NoData>
+        <NoData></NoData>
       </div>
       <PanelItem v-for="groupMember in selectedGroup.groupMember" :display-name="resolveUserName(groupMember.identity)"
         :id="groupMember.identity.identity" :uuid="groupMember.identity.identityID" :show-avatar="true" :show-bar="true"
@@ -16,7 +18,7 @@
 <script setup lang="ts">
 import { ModelsGroup, ModelsIdentity } from '@/generated/api';
 import ViewPanelTemplate from './ViewPanelTemplate.vue'
-import { watch, ref } from 'vue'
+import { ref } from 'vue'
 import { PropType } from 'vue'
 import PanelItem from './PanelItem.vue';
 import NoData from './NoData.vue';
@@ -28,14 +30,6 @@ const props = defineProps({
   selectedGroup: { type: Object as PropType<ModelsGroup>, required: false, default: () => [] }
 })
 
-watch(() => props.selectedGroup, (newGroupName) => {
-  console.log("Selected group changed to", newGroupName)
-  newGroupName.groupMember.forEach(member => {
-    console.log("Member:", member.identity, "Messages:", member.messageCount)
-  });
-
-})
-
 function resolveUserName(identity: ModelsIdentity): string {
   return identity.nickName || identity.firstName || identity.lastName || identity.identity;
 }
@@ -45,7 +39,6 @@ function isSelected(identityID: string): boolean {
 }
 
 function handleItemClick(id: string) {
-  console.log("Clicked member:", id)
   const index = selectedMembers.value.indexOf(id)
   if (index !== -1) {
     selectedMembers.value.splice(index, 1)
--- frontend/analyzer-frontend/src/service/ApiService.ts
+++ frontend/analyzer-frontend/src/service/ApiService.ts
@@ -15,13 +15,10 @@ const defaultApi = new DefaultApi(configuration);
 export async function loadGroupTimeline(
   groupName: string
 ): Promise<ModelsGroupTimeline[]> {
-  console.log("Loading timeline data for group:", groupName);
-
   try {
     const response = await defaultApi.groupsTimelineGet({ group: groupName });
     return response;
   } catch (err) {
-    console.error("Error loading timeline:", err);
     return [];
   }
 }
@@ -29,15 +26,12 @@ export async function loadGroupTimeline(
 export async function loadContactTimeline(
   userId: string
 ): Promise<ModelsContactTimeline[]> {
-  console.log("Loading timeline data for contact:", userId);
-
   try {
     const response = await defaultApi.contactsTimelineGet({
       userId: userId,
     });
     return response;
   } catch (err) {
-    console.error("Error loading timeline:", err);
     return [];
   }
 }
@@ -49,10 +43,8 @@ export async function loadWordCloudData(): Promise<ModelsWordCount[]> {
 export async function loadContacts(): Promise<ModelsContact[]> {
   try {
     const response = await fetch("http://localhost:8080/contacts");
-    console.log("Contacts response:", response);
     return response.json();
   } catch (err) {
-    console.error("Error loading contacts:", err);
     return [];
   }
 }
